1a86342b188f87d43070b95e740ea5914a7bcffe
yeo <yeoyaya1@gmail.com>
2019-09-30 13:47:53 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    $name ="yeo";
+    var_dump($name);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <h2>BONJOUR</h2>
+    </div>
+</body>
+</html>
